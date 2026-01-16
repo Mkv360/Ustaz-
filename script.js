648a59d98bc20d4b65aecdf4e404e0a4148e335a
Mkv360 <125382027+Mkv360@users.noreply.github.com>
2026-01-16 18:09:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-const tg = window.Telegram.WebApp;
-tg.ready();
+function goSignup() {
+  window.location.href = "signup.html";
+}
 
-function flipCard() {
-  document.getElementById("card").classList.toggle("flip");
+function goHome() {
+  window.location.href = "home.html";
+}
+
+function showParent() {
+  document.getElementById("parentForm").classList.remove("hidden");
+  document.getElementById("ustazForm").classList.add("hidden");
+}
+
+function showUstaz() {
+  document.getElementById("ustazForm").classList.remove("hidden");
+  document.getElementById("parentForm").classList.add("hidden");
+}
+
+function toggleMenu() {
+  const menu = document.getElementById("menu");
+  menu.style.left = menu.style.left === "0px" ? "-200px" : "0px";
 }
